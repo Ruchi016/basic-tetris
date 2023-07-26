4ef0c6d384edebb1f30181a73889401a40740548
Ruchi016 <dasruchika016@gmail.com>
2023-07-26 18:58:41 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    function alertMe(){
+        wlcm_msg="🎮"
+        alert("Welcome to the world of Tetris!"+wlcm_msg)
+       }
+       alertMe()
    const grid = document.querySelector('.grid')
    let squares = Array.from(document.querySelectorAll('.grid div'))
    const ScoreDisplay = document.querySelector('#score')
    const StartBtn = document.querySelector('#start-button')
    const width = 10
    
-   
+   //The Tetrominoes
+   const lTetromino = [
+    [1, width+1, width+2+1, 2],
 
+   ]
+
+   
 })
